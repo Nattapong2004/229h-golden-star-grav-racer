cc8b18c57974c0f0d827cae136acd3d7a8ea8c17
Nattapong2004 <nattapong.buph@bumail.net>
2025-03-30 18:51:21 +0700

    save

--- Assets/Scripts/CreditsManager.cs
+++ Assets/Scripts/CreditsManager.cs
@@ -1,21 +1,21 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CreditsManager : MonoBehaviour
 {
-    public Text creditsText; // ลาก CreditsText มาใส่ใน Inspector
+    public TextMeshProUGUI creditsText; // ลาก CreditsText มาใส่ใน Inspector
 
     void Start()
     {
         // ตั้งค่าข้อความ Credit
         creditsText.text =
-            "ทีมพัฒนาเกม:\n\n" +
-            "1. รหัส 12345 ชื่อ-นามสกุล Section 01 เลขที่ 1 - โปรแกรมเมอร์\n" +
+            "1.ID 1660706241 Nattapong Buphu Section 229H No.18" +
+            "()"+
             "2. รหัส 67890 ชื่อ-นามสกุล Section 01 เลขที่ 2 - ออกแบบเกม\n" +
-            "3. รหัส 54321 ชื่อ-นามสกุล Section 01 เลขที่ 3 - ทดสอบเกม\n\n" +
-            "Assets ที่ใช้:\n" +
-            "- เสียงจาก FreeSound.org\n" +
-            "- ภาพจาก Unity Asset Store\n\n" +
-            "ขอบคุณที่เล่นเกมของเรา!";
+            
+            "Assets : " +
+           
+            "Tankyou";
     }
 }
